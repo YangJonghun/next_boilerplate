983b2c2bd6cde22f8bedada6cffdeeb286006988
YangJH <jhaugusty@gmail.com>
2019-06-14 02:36:15 +0900

    add base components
    - _app.tsx
    - _document.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import App, { Container, NextAppContext } from 'next/app';
+
+import Helmet from '../components/Helmet';
+
+class CustomApp extends App {
+  static async getInitialProps(context: NextAppContext) {
+    const { Component, ctx } = context;
+
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Helmet title="Next-Boilerplate" />
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default CustomApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  public render() {
+    return (
+      <html lang="ko" dir="ltr">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+          <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
+          <link rel="apple-touch-icon" href="/static/touch-icon.png" />
+          <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
+          <link rel="icon" href="/static/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default CustomDocument;
